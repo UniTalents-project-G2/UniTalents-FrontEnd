ccd6b4f8f242f03552646850665b6d3243468dd6
WoodsDos <javier.gonzales.2202@gmail.com>
2025-05-16 21:46:04 -0500

    fix: solve f5 problem

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { copyFileSync } from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    {
+      name: 'copy-staticwebapp-config',
+      closeBundle() {
+        // Copia el archivo al final del build
+        copyFileSync('staticwebapp.config.json', 'dist/staticwebapp.config.json')
+      }
+    }
   ],
   resolve: {
     alias: {
